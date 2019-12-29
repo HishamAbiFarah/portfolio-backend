3389faf125ffc549994ebb45784318504b39d985
Hisham Abi Farah <hisham.abi.farah@gmail.com>
2019-12-29 13:56:16 +0200

    added pagination to skills route

--- server/routes/skills.js
+++ server/routes/skills.js
@@ -2,15 +2,48 @@ const express = require('express');
 const router = express.Router();
 const { Skill, validate } = require('../models/skill');
 
-
 router.get('/', async (req, res) => {
+
     const skills = await Skill
         .find()
-        .sort({ name: 1 })
-        .select({ name: 1 })
-        .limit(10)
-    res.status(200).send(skills);
+        .select({ name: 1, date: 1 })
+
+    const pageCount = Math.ceil(skills.length / 5);
+
+    let page = parseInt(req.query.page);
+
+    if (!page || page === 1) {
+        page = 1
+    } else {
+        if (page <= pageCount) {
+            page
+        } else {
+            return res.json({
+                "status" : 204,
+                "message": "end of records reached",
+                "skills": []
+            });
+        }
+    }
+    res.status(200).json({
+        "message" : "success",
+        "status" : 200,
+        "totalRecords" : skills.length,
+        "page": page,
+        "pageCount": pageCount,
+        "skills": skills.slice(page * 5 - 5, page * 5)
+    });
 });
+
+
+// router.get('/', async (req, res) => {
+//     const skills = await Skill
+//         .find()
+//         .sort({ name: 1 })
+//         .select({ name: 1 })
+//         .limit(3)
+//     res.status(200).send(skills);
+// });
 
 router.post('/', async (req, res) => {
     const { error } = validate(req.body)
